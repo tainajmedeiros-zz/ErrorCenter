bfada5e65eaac9fa3db7ae8997dbd5879056835a
tainajmedeiros <tainajmedeiros@gmail.com>
2021-02-20 12:48:25 -0300

    feat: creat secutiry and config logic

---
+++ src/main/java/br/com/report/config/SwaggerConfig.java
@@ -0,0 +1,66 @@
+package br.com.report.config;
+
+import com.google.common.net.HttpHeaders;
+import io.swagger.models.auth.In;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket placeApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.report"))
+				.paths(regex("/api.*"))
+				.build()
+				.apiInfo(metaInfo())
+				.securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+				.securityContexts(Arrays.asList(securityContext()));
+	}
+
+	private ApiInfo metaInfo() {
+		ApiInfo apiInfo =  new ApiInfo(
+				"Central de Erros",
+				"API REST desenvolvida para monitorar uma central de erros.",
+				"1.0",
+				"Terms Of Service",
+				new Contact("Tainá Medeiros", "", "tainajmedeiros@gmail.com"),
+				"Apache License Version 2.0",
+				"https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+		);
+
+		return apiInfo;
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.forPaths(PathSelectors.ant("/pessoa/**"))
+				.build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope
+				= new AuthorizationScope("ADMIN", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(
+				new SecurityReference("Token Access", authorizationScopes));
+	}
+}
